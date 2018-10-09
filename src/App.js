4d032117020687730a6db7aed6e696aa558e59a6
Kris Kamikawa <kris.kamikawa@gmail.com>
2018-10-08 21:58:25 -0600

    Add react router

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React, { Component } from "react";
 import Header from "./components/Header";
+import Home from "./components/Home";
+import Books from "./components/Books";
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
-      <div>
-        <Header />
-      </div>
+
+      <Router>
+        <div>
+          <Header />
+          
+          <Route exact path="/" component={Home}/>
+          <Route exact path="/books" component={Books}/>
+          {/* <Route exact path="/authors" component={Authors}/>
+          <Route path="/books/:id" component={Book}/>
+          <Route path ="authors/:id" component={Author}/>
+          <Route path="/books/new" component={NewBook}/>  
+          <Route path ="books/:id/edit" component={EditBook}/>
+          <Route path ="books/:id/delete" component={DeleteBook}/>
+          <Route path="/authors/new" component={NewAuthor}/>  
+          <Route path ="authors/:id/edit" component={EditAuthor}/>
+          <Route path ="authors/:id/delete" component={DeleteAuthor}/> */}
+        </div>
+      </Router>
+
     );
   }
 }
---
+++ src/components/Books.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Books = () => {
+
+  return (
+    <p>Books page here</p>
+  )
+
+}
+
+export default Books;
